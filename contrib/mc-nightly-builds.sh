600d4d3f35d626064eb1d9fee69fac98adcef838
Andrey Tataranovich <tataranovich@gmail.com>
2011-12-22 15:00:00 +0200

    Build necessity check moved to mc-nightly-builds.sh

--- contrib/mc-nightly-builds.sh
+++ contrib/mc-nightly-builds.sh
@@ -5,28 +5,23 @@ echo "Started at `date -R`"
 
 [ -x ~/bin/prepare-environment.sh ] && ~/bin/prepare-environment.sh
 
-if [ -r /home/buildbot/.series/nightly -a -d /home/buildbot/mc-git ]; then
-	pushd /home/buildbot/mc-git >/dev/null
-	LAST_GIT_COMMIT=`git log -n 1 | grep ^commit | head -n 1 | awk '{print $2}'`
-	grep -q "^$LAST_GIT_COMMIT" /home/buildbot/.series/nightly && {
-		echo
-		echo "Nightly build for commit $LAST_GIT_COMMIT already done."
-		echo "Remove /home/buildbot/.series/nightly if you want to force build"
-		echo
-		echo "Successfully ended at `date -R`"
-		exit 0
-	}
-	popd
-fi
-
 # Try to build with buildbot
 sudo -u buildbot /home/buildbot/buildbot.sh --nightly >$HOME/tmp/nightly-builds.log 2>&1
 
-if [ $? != 0 ]; then
-	[ -f $HOME/tmp/nightly-builds.log ] && grep ^BUILDBOT $HOME/tmp/nightly-builds.log
-	echo "Error while building project, skipping repo update"
-	exit 1
-fi
+case $? in
+	0)
+		echo "Build completed without error, starting repository update"
+		break
+		;;
+	10) echo "Successfully ended at `date -R`"
+		exit 0
+		;;
+	*)
+		[ -f $HOME/tmp/nightly-builds.log ] && grep ^BUILDBOT $HOME/tmp/nightly-builds.log
+		echo "Error while building project, skipping repo update"
+		exit 1
+		;;
+esac
 
 [ -d /home/buildbot/distribution ] && rsync -a /home/buildbot/distribution/ $HOME/my-local-repo/
 $HOME/bin/local-repo-update >$HOME/tmp/nightly-repo-update.log 2>&1
