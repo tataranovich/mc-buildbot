c8dc060341019d9c49fec2b8e73ec437fa1eea7a
Andrey Tataranovich <tataranovich@gmail.com>
2020-02-05 23:15:30 +0300

    Added Ubuntu Eoan.
    Deleted Ubuntu Cosmic and Ubuntu Disco due to EOL.
    Import the latest changes.

--- contrib/arm-builds/arm-build.sh
+++ contrib/arm-builds/arm-build.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
 
 DSC_FILE="$1"
-if [ -z "$DSC_FILE" ]; then
-    echo "Specify package file name" >&2
-    exit 1
-fi
+TARGET="$2"
 
-egrep -v '^($|\s+?#)' arm.list | while IFS=: read OS DIST ARCH
-do
-    echo -n "$OS-$DIST-$ARCH: "
-    sudo OS=$OS DIST=$DIST ARCH=$ARCH pbuilder --build --configfile /etc/pbuilder/debbuilder "$DSC_FILE" >"build-${OS}-${DIST}-${ARCH}.log" 2>&1
+build_target() {
+    local _OS _DIST _ARCH
+    _OS="$1"
+    _DIST="$2"
+    _ARCH="$3"
+    echo -n "${_OS}-${_DIST}-${_ARCH}: "
+    if [ ! -f "/var/cache/pbuilder/${_OS}-${_DIST}-${_ARCH}-base.tgz" ]; then
+        sudo OS=$_OS DIST=$_DIST ARCH=$_ARCH pbuilder --create --configfile /etc/pbuilder/debbuilder >"logs/${_OS}-${_DIST}-${_ARCH}.create.log" 2>&1
+    fi
+    sudo OS=$_OS DIST=$_DIST ARCH=$_ARCH pbuilder --build --configfile /etc/pbuilder/debbuilder "$DSC_FILE" >"logs/${_OS}-${_DIST}-${_ARCH}.build.log" 2>&1
     if [ $? != 0 ]; then
         echo FAIL
     else
         echo OK
     fi
-done
+}
+
+if [ -z "$DSC_FILE" ]; then
+    echo "Specify package file name" >&2
+    exit 1
+fi
+
+if [ ! -f "$DSC_FILE" ]; then
+    echo "File not found: $DSC_FILE" >&2
+    exit 1
+fi
+
+if [ -z "$TARGET" ]; then
+    egrep -v '^($|\s+?#)' arm.list | while IFS=: read OS DIST ARCH
+    do
+        build_target $OS $DIST $ARCH
+    done
+else
+    OS=$(echo "$TARGET" | cut -d: -f1)
+    DIST=$(echo "$TARGET" | cut -d: -f2)
+    ARCH=$(echo "$TARGET" | cut -d: -f3)
+    build_target $OS $DIST $ARCH
+fi
